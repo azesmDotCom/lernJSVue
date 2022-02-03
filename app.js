8bdcceff3ed7166782aad23ffbf4c4edfb6be181
azesm <azesm@yahoo.com>
2022-02-03 11:07:21 +0300

    number

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ const user = {
     age: 30
 };
 user.age = 41;
- console.log(user); */
+ console.log(user); 
 
 // 8. преобразование типов
 
@@ -148,3 +148,40 @@ value = Boolean([]);
 
 console.log(value);
 console.log(typeof value);
+*/
+// Numbers
+
+const num1 = 10;
+const num2 = 20;
+let value;
+
+// + * / - %
+value = num1 + num2;
+value = value + 100;
+value += 100;
+//value = 5 % 2;
+value++;
+value--;
+++value;
+--value;
+
+
+value = 0.6 + 0.7;
+//value = parseFloat(value.toFixed(1));
+value = ( 0.6 * 10 + 0.7 * 10 ) / 10;
+
+// Math
+value = Math.PI;
+value = Math.random(); 
+value = Math.round(2.9);
+value = Math.ceil(2.1);
+value = Math.floor(2.9);
+value = Math.min(22, 14, 128, 3, 968);
+value = Math.floor(Math.random() * 10 + 1);
+
+const arr = ['black', 'red', 'blue', 'orange', 'pink', 'green'];
+value = Math.floor(Math.random() * arr.length);
+
+console.log(arr[value]);
+
+console.log(value);
